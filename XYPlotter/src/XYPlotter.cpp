a8e776bf2d6dddc5a116f2c3a0f2cada80587956
Tuomas Autio <tautio@verifa.io>
2020-09-29 12:05:07 +0300

    bresenham and prepare motor task

--- XYPlotter/src/XYPlotter.cpp
+++ XYPlotter/src/XYPlotter.cpp
@@ -1,11 +1,11 @@
 /*
-===============================================================================
+ ===============================================================================
  Name        : main.c
  Author      : Lauri.M Tuomas.A Taneli.T
  Version     :
  Copyright   : $(copyright)
  Description : main definition
-===============================================================================
+ ===============================================================================
  */
 
 #if defined (__USE_LPCOPEN)
@@ -17,11 +17,15 @@
 #endif
 
 #include <cr_section_macros.h>
-
+#include <stdio.h>
+#include <ctype.h>
+#include "task.h"
+#include "tools/GetUartString.h"
 #include "FreeRTOS.h"
+#include "tools/Parser.h"
 
-static void prvSetupHardware(void)
-{
+
+static void prvSetupHardware(void) {
 	SystemCoreClockUpdate();
 	Board_Init();
 
@@ -29,7 +33,8 @@ static void prvSetupHardware(void)
 	Board_LED_Set(0, false);
 }
 
-
+Parser parse;
+GetUartString uartReader('\n');
 
 /*****************************************************************************
  * Public functions
@@ -38,7 +43,7 @@ static void prvSetupHardware(void)
 /* the following is required if runtime statistics are to be collected */
 extern "C" {
 
-void vConfigureTimerForRunTimeStats( void ) {
+void vConfigureTimerForRunTimeStats(void) {
 	Chip_SCT_Init(LPC_SCTSMALL1);
 	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
 	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
@@ -47,26 +52,64 @@ void vConfigureTimerForRunTimeStats( void ) {
 }
 /* end runtime statictics collection */
 
-/**
- * @brief
- * @return	Nothing, function should not exit
- */
+void bresenham(float x0, float y0, float x1, float y1) {
+	auto dx = x1-x0;
+	auto dy = y1-y0;
 
-#include <stdio.h>
-#include <ctype.h>
-#include "task.h"
-#include "tools/UartController.h"
+	auto max = std::max(std::abs(dx),std::abs(dy));
+
+	auto deltaX = std::abs(dx) / max;
+	auto deltaY = std::abs(dy) / max;
+}
+
+void calibrate() {
+
+}
+
+void worker() {
+	char *str;
+	float lastX;
+	float lastY;
+
+	while (1) {
+		uartReader.getUartMessage(str);
+		auto cmd = parse.parse(str);
+
+		if (cmd.type == COMMAND_MOVE) {
+			//motor move
+			bresenham(lastX,lastY,cmd.x,cmd.y);
+
+		} else if (cmd.type == COMMAND_PEN) {
+			if (cmd.penvalue != 100) {
+				//Move pen
+			} else {
+				//move Pen
+			}
+		} else if (cmd.type == COMMAND_LASER) {
+			//laser power
+		} else if (cmd.type == COMMAND_START) {
+			//start and calibrate
+			calibrate();
+			lastX = 0;
+			lastY = 0;
+		} else if (cmd.type == COMMAND_ORIGIN) {
+			//Motor move 0,0
+		} else if (cmd.type == COMMAND_READY) {
+			//ready
+		}
+	}
+}
 
 int main(void) {
 	//vTaskStartScheduler();
 	prvSetupHardware();
 
-	UartController URT('\n');
+	GetUartString URT('\n');
 	char buff[64];
-	while(1){
+	while (1) {
 		URT.getUartMessageFromFile(buff);
 
 		Board_UARTPutSTR(buff);
 	}
-	return 0 ;
+	return 0;
 }
